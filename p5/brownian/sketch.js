e4140d461de57a5a93bc9e79e5046adf7c124bb3
pbenson <pbenson@me.com>
2016-09-15 11:53:19 -0400

    made brownian fit the screen

--- p5/brownian/sketch.js
+++ p5/brownian/sketch.js
@@ -1,10 +1,10 @@
 "use strict";
 
 var needsRedraw = true;
+var cnv;
 
 function setup() {
-  createCanvas(1024, 256);
-  background(230);
+  centerCanvas();
   noFill();
   frameRate(10);
 }
@@ -13,9 +13,9 @@ function draw() {
   translate(0, height * 0.5);
   translate(1, -1);
   var xStep = 2;
-  var vol = 2;
+  var vol = height * 0.01;
   var y = 0;
-  var grayLevel =  random() * 256;
+  var grayLevel = random() * 256;
   stroke(grayLevel, grayLevel, grayLevel, 100);
   beginShape();
   for (var x = 0; x < width; x += xStep) {
@@ -23,4 +23,16 @@ function draw() {
     y += randomGaussian() * vol;
   }
   endShape();
+}
+
+function centerCanvas() {
+  cnv = createCanvas(windowWidth - 40 , windowHeight - 40);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  background(230);
+}
+
+function windowResized() {
+  centerCanvas();
 }
