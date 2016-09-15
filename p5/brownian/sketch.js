d7ee94446ef863a09b1190b9ec148a366382c44e
pbenson <pbenson@me.com>
2016-09-15 12:04:43 -0400

    adjusted background color of brownian

--- p5/brownian/sketch.js
+++ p5/brownian/sketch.js
@@ -15,7 +15,7 @@ function draw() {
   var xStep = 2;
   var vol = height * 0.01;
   var y = 0;
-  var grayLevel = random() * 256;
+  var grayLevel = 192 + random() * 64;
   stroke(grayLevel, grayLevel, grayLevel, 100);
   beginShape();
   for (var x = 0; x < width; x += xStep) {
@@ -30,7 +30,7 @@ function centerCanvas() {
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
-  background(230);
+  background(240);
 }
 
 function windowResized() {
