15c37e1cec7ba5916650df2953f9edee4115f4d8
pbenson <pbenson@me.com>
2016-09-26 14:22:40 -0400

    route.js update

--- p5/tsp-euclidean/route.js
+++ p5/tsp-euclidean/route.js
@@ -30,7 +30,22 @@ class Route {
     return c;
   }
 
-  draw(rowLabel, minCost, minExpectedCost, x, y) {
+  expectedCostNoReturn() {
+    var c = edgeMatrix[nodeZero.label][this.nodeSequence[1].label].cost();
+    var probNotFound = 1;
+    for (var idx = 1; idx < this.nodeSequence.length - 2; ++idx) {
+      var currentNode = this.nodeSequence[idx];
+      var edgeToNext = edgeMatrix[currentNode.label][this.nodeSequence[idx + 1].label];
+      var edgeToZero = edgeMatrix[currentNode.label][nodeZero.label];
+      var probFound = currentNode.p / probNotFound;
+      var probNotFoundSoFar = probNotFound;
+      probNotFound -= currentNode.p;
+      c += edgeToNext.cost() * probNotFound + edgeToZero.cost() * probNotFoundSoFar * probFound;
+    }
+    return c;
+  }
+
+  draw(rowLabel, minCost, minExpectedCost, minExpectedCostNoReturn, x, y) {
     fill(0);
     noStroke();
     textStyle(NORMAL);
@@ -40,7 +55,9 @@ class Route {
     text(this.cost().toFixed(0), x + 40, y);
     this.expectedCost() <= minExpectedCost ? fill(255, 100, 100) : fill(0);
     text(this.expectedCost().toFixed(0), x+80, y);
-    text(this.nodeSequence.toString(), x+140, y);
+    this.expectedCostNoReturn() <= minExpectedCostNoReturn ? fill(255, 100, 100) : fill(0);
+    text(this.expectedCostNoReturn().toFixed(0), x+140, y);
+    text(this.nodeSequence.toString(), x+200, y);
   }
 
   drawHeader(x, y) {
@@ -49,8 +66,9 @@ class Route {
     textStyle(BOLD);
     text('row', x, y);
     text('cost', x+30, y);
-    text('E[cost]', x+70, y);
-    text('sequence', x+135, y);
+    text('E[c]', x+80, y);
+    text('E[c](no ret)', x+125, y);
+    text('sequence', x+210, y);
   }
 
 }
